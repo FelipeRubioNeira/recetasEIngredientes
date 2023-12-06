431d35940009bb110d2b6ba7bb5f1594b0209914
Felipe Eduardo Rubio <felipeeduardorubio@MacBook-Pro-de-Felipe-2.local>
2023-12-06 07:46:48 -0300

    se ha agregado el buscador y el filtro en la pantalla de recetas. Falta que al presionar el boton de filtro se muestre un modal con el listado de ingredientes

--- app/src/main/java/com/app/recetasEIngredientes/mainMenu/recetas/ListadoRecetasView.kt
+++ app/src/main/java/com/app/recetasEIngredientes/mainMenu/recetas/ListadoRecetasView.kt
@@ -1,35 +1,56 @@
 package com.app.recetasEIngredientes.mainMenu.recetas
 
+import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.app.recetasEIngredientes.R
 import com.app.recetasEIngredientes.common.componentes.BotonAgregar
 import com.app.recetasEIngredientes.constantes.Colores
+import com.app.recetasEIngredientes.constantes.Fuentes
 
 @Composable
 fun ListadoRecetasView() {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
+            .padding(16.dp)
     ) {
+
+        Buscador()
 
         ListadoRecetas()
 
@@ -39,56 +60,94 @@ fun ListadoRecetasView() {
             onPress = { }
         )
     }
+
+
 }
 
 @Composable
 fun ListadoRecetas() {
 
-    Buscador()
 
 }
 
 @Composable
 fun Buscador() {
 
-    Row() {
-        Busqueda()
-        Filtro()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Busqueda(Modifier.weight(9f))
+        Spacer(modifier = Modifier.width(8.dp))
+        Filtro(Modifier.weight(1f))
     }
 }
 
 @Preview
 @Composable
-fun Busqueda() {
+fun Busqueda(modifier: Modifier = Modifier) {
+
+    var value by rememberSaveable { mutableStateOf("") }
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
             .height(50.dp)
-            .border(1.dp, Colores.GRIS_OSCURO, shape = RoundedCornerShape(8.dp))
+            .fillMaxWidth()
     ) {
-        TextField(value = "", onValueChange = {})
 
-        //BotonBuscar(onPress = {})
+        TextField(
+            value = value,
+            onValueChange = { value = it },
+            textStyle = TextStyle(
+                fontFamily = Fuentes.REM_LIGHT,
+                color = Colores.NEGRO,
+                fontSize = 16.sp
+            ),
+            label = null,
+            singleLine = true,
+            placeholder = { Text(text = "Buscar", fontFamily = Fuentes.REM_LIGHT) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Colores.ROJO,
+                unfocusedBorderColor = Colores.GRIS_OSCURO,
+                cursorColor = Colores.GRIS_OSCURO,
+                focusedLabelColor = Colores.GRIS_OSCURO,
+            ),
+
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        modifier = Modifier.size(40.dp),
+                        tint = Colores.GRIS_OSCURO,
+                        painter = painterResource(id = R.drawable.ic_search),
+                        contentDescription = "icono de busqueda"
+                    )
+                }
+
+            },
+
+            modifier = Modifier.fillMaxWidth(),
+
+            )
     }
 
 }
 
 @Composable
-fun BotonBuscar(
-    onPress: () -> Unit = {}
-) {
-    IconButton(onClick = { onPress() }) {
+fun Filtro(modifier: Modifier = Modifier) {
+
+    IconButton(
+        modifier = modifier,
+        onClick = { }
+    ) {
         Icon(
+            painter = painterResource(id = R.drawable.ic_tune),
             modifier = Modifier.size(40.dp),
             tint = Colores.GRIS_OSCURO,
-            painter = painterResource(id = R.drawable.search_ic),
-            contentDescription = "icono de busqueda"
+            contentDescription = "icono de filtro"
         )
     }
-}
-
-@Composable
-fun Filtro() {
 
 }
